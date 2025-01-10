d599a4f36ff8d32068b1cecc1bb2e48c97e45cc5
christo <christopher.pkm@gmail.com>
2025-01-10 15:37:10 +0000

    init commit

---
+++ src/index.test.ts
@@ -0,0 +1,81 @@
+import { check3by3BoxIsValid, checkColumnIsValid, checkRowIsValid } from ".";
+
+const puzzleInputA: number[][] = [
+  [9, 1, 5, -1, -1, 3, 4, -1, 6],
+  [-1, -1, -1, 1, -1, 2, -1, 8, 9],
+  [-1, 6, -1, -1, -1, 4, 7, -1, 3],
+
+  [-1, -1, -1, 3, 1, -1, -1, 9, -1],
+  [5, -1, 8, -1, 4, -1, -1, 3, 2],
+  [3, 4, 1, 8, -1, -1, -1, 5, -1],
+
+  [-1, -1, -1, 4, 9, 6, -1, -1, -1],
+  [2, 7, -1, -1, -1, -1, 9, -1, -1],
+  [4, -1, 9, -1, -1, 1, 3, -1, 5],
+];
+
+const puzzleInputB: number[][] = [
+  [9, 1, 5, 1, 2, 3, 4, 4, 6],
+  [-1, -1, -1, 1, -1, 2, -1, 8, 9],
+  [-1, 6, -1, -1, -1, 4, 7, -1, 3],
+
+  [-1, -1, -1, 3, 1, -1, -1, 9, -1],
+  [5, -1, 8, -1, 3, 1, -1, 3, 2],
+  [3, 4, 1, 8, -1, -1, -1, 3, -1],
+
+  [-1, -1, -1, 4, 9, 6, -1, -1, -1],
+  [2, 7, -1, -1, -1, -1, 9, -1, -1],
+  [9, -1, 9, -1, -1, 1, 3, -1, 5],
+];
+
+describe("checkRowIsValid", () => {
+  test("first row of puzzle input a is valid", () => {
+    expect(checkRowIsValid(0, puzzleInputA)).toBe(true);
+  });
+
+  test("second row of puzzle input a is valid", () => {
+    expect(checkRowIsValid(1, puzzleInputA)).toBe(true);
+  });
+
+  test("first row of puzzle input b is invalid", () => {
+    expect(checkRowIsValid(0, puzzleInputB)).toBe(false);
+  });
+  test("sixth row of puzzle input b is invalid", () => {
+    expect(checkRowIsValid(5, puzzleInputB)).toBe(false);
+  });
+});
+
+describe("checkColumnIsValid", () => {
+  test("first column of puzzle input a is valid", () => {
+    expect(checkColumnIsValid(0, puzzleInputA)).toBe(true);
+  });
+
+  test("second column of puzzle input a is valid", () => {
+    expect(checkColumnIsValid(1, puzzleInputA)).toBe(true);
+  });
+
+  test("first column of puzzle input b is invalid", () => {
+    expect(checkColumnIsValid(0, puzzleInputB)).toBe(false);
+  });
+  test("sixth column of puzzle input b is invalid", () => {
+    expect(checkColumnIsValid(5, puzzleInputB)).toBe(false);
+  });
+});
+
+
+describe("check3by3BoxIsValid", () => {
+    test("top left 3 by 3 box of puzzle input a is valid", () => {
+      expect(check3by3BoxIsValid(0,0, puzzleInputA)).toBe(true);
+    });
+  
+    test("top middle 3 by 3 box puzzle input a is valid", () => {
+      expect(check3by3BoxIsValid(3,1, puzzleInputA)).toBe(true);
+    });
+  
+    test("top right 3 by 3 box puzzle input b is invalid", () => {
+      expect(checkColumnIsValid(0, puzzleInputB)).toBe(false);
+    });
+    test("middle middle 3 by 3 box puzzle input b is invalid", () => {
+      expect(checkColumnIsValid(5, puzzleInputB)).toBe(false);
+    });
+  });
---
+++ src/index.ts
@@ -0,0 +1,185 @@
+export const checkRowIsValid = (yIndex: number, arr2d: number[][]) => {
+  let foundNumbersInRow: number[] = [];
+  for (let x = 0; x < 9; x++) {
+    if (arr2d[yIndex][x] === -1) {
+      continue;
+    }
+
+    if (foundNumbersInRow.includes(arr2d[yIndex][x])) {
+      return false;
+    }
+    foundNumbersInRow.push(arr2d[yIndex][x]);
+  }
+  return true;
+};
+
+export const checkColumnIsValid = (xIndex: number, arr2d: number[][]) => {
+  let foundNumbersInColumn: number[] = [];
+  for (let y = 0; y < 9; y++) {
+    if (arr2d[y][xIndex] === -1) {
+      continue;
+    }
+
+    if (foundNumbersInColumn.includes(arr2d[y][xIndex])) {
+      return false;
+    }
+    foundNumbersInColumn.push(arr2d[y][xIndex]);
+  }
+  return true;
+};
+
+export const check3by3BoxIsValid = (
+  xIndex: number,
+  yIndex: number,
+  arr2d: number[][]
+) => {
+  let found: number[] = [];
+
+  const checkRowWithin3by3blockIsValid = (xIndexOffset: number, y: number) => {
+    let xIndexWithin3by3Box = xIndexOffset % 3;
+    // console.log("Y", y);
+
+    // console.log("found", found);
+
+    if (xIndexWithin3by3Box === 0) {
+      for (let x = 0; x < 3; x++) {
+        if (arr2d[y][xIndexOffset + x] === -1) {
+          continue;
+        }
+
+        if (found.includes(arr2d[y][xIndexOffset + x])) {
+          return false;
+        }
+
+        found.push(arr2d[y][xIndexOffset + x]);
+      }
+    } else if (xIndexWithin3by3Box === 1) {
+      for (let x = -1; x <= 1; x++) {
+        if (arr2d[y][xIndexOffset + x] === -1) {
+          continue;
+        }
+
+        if (found.includes(arr2d[y][xIndexOffset + x])) {
+          return false;
+        }
+        found.push(arr2d[y][xIndexOffset + x]);
+      }
+    } else if (xIndexWithin3by3Box === 2) {
+      for (let x = -2; x <= 0; x++) {
+        if (arr2d[y][xIndexOffset + x] === -1) {
+          continue;
+        }
+
+        if (found.includes(arr2d[y][xIndexOffset + x])) {
+          return false;
+        }
+        found.push(arr2d[y][xIndexOffset + x]);
+      }
+    }
+    return true;
+  };
+
+  let yIndexWithin3by3Box = yIndex % 3;
+
+  if (yIndexWithin3by3Box === 0) {
+    for (let y = 0; y < 3; y++) {
+      if (!checkRowWithin3by3blockIsValid(xIndex, yIndex + y)) return false;
+    }
+  } else if (yIndexWithin3by3Box === 1) {
+    for (let y = -1; y <= 1; y++) {
+      if (!checkRowWithin3by3blockIsValid(xIndex, yIndex + y)) return false;
+    }
+  } else if (yIndexWithin3by3Box === 2) {
+    for (let y = -2; y <= 0; y++) {
+      if (!checkRowWithin3by3blockIsValid(xIndex, yIndex + y)) return false;
+    }
+  }
+
+  return true;
+};
+
+// debug this function
+const numberAtPosXYIsLegal = (
+  xIndex: number,
+  yIndex: number,
+  arr2d: number[][]
+) => {
+  // console.log("running check");
+  return (
+    checkRowIsValid(yIndex, arr2d) &&
+    checkColumnIsValid(xIndex, arr2d) &&
+    check3by3BoxIsValid(xIndex, yIndex, arr2d)
+  );
+};
+
+// @ts-ignore
+const solvePropperSudoku = (
+  x: number,
+  y: number,
+  arr2d: number[][]
+): number[][] | undefined => {
+  console.log("fn ran", x, y, arr2d);
+  if (arr2d[y][x] !== -1) {
+    // if location is a clue
+    // console.log(`location ${x}, ${y}, is clue"`);
+     solvePropperSudoku(x == 8 ? 0 : x + 1, x == 8 ? y + 1 : y, arr2d);
+     console.log("back", x, y, arr2d);
+
+  } else {
+    for (let i = 1; i <= 9; i++) {
+      let arr2dcopy = Array.from(arr2d, (arr) => arr.slice());
+
+      // if(arr2dcopy[y][x] === -1)
+      //     continue;
+
+      arr2dcopy[y][x] = i;
+      if (y === 8 && x === 8) {
+        // end condition
+        // console.log("end condition", arr2dcopy);
+        return arr2dcopy;
+      } else {
+        // console.log("block running");
+        let result: number[][] | undefined = undefined;
+
+        // console.log("i", i);
+        if (numberAtPosXYIsLegal(x, y, arr2dcopy)) {
+          // console.log("exe");
+          result = solvePropperSudoku(
+            x == 8 ? 0 : x + 1,
+            x == 8 ? y + 1 : y,
+            arr2dcopy
+          );
+        }
+
+        // console.log("result", result);
+
+        if (result) {
+          // return result;
+        } else {
+          // return null;
+        }
+        // if number in position isn't legal continue to next number for this slot -
+        // or if next slot didn't find any number that worked for it
+      }
+    }
+  }
+
+  // if this is reached backtrack will happen
+};
+
+const puzzleInput: number[][] = [
+  [9, 1, 5, -1, -1, 3, 4, -1, 6],
+  [-1, -1, -1, 1, -1, 2, -1, 8, 9],
+  [-1, 6, -1, -1, -1, 4, 7, -1, 3],
+
+  [-1, -1, -1, 3, 1, -1, -1, 9, -1],
+  [5, -1, 8, -1, 4, -1, -1, 3, 2],
+  [3, 4, 1, 8, -1, -1, -1, 5, -1],
+
+  [-1, -1, -1, 4, 9, 6, -1, -1, -1],
+  [2, 7, -1, -1, -1, -1, 9, -1, -1],
+  [4, -1, 9, -1, -1, 1, 3, -1, 5],
+];
+
+const solvedSudoku = solvePropperSudoku(0, 0, puzzleInput);
+console.log("final result", solvedSudoku);
