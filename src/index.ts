fd0143fe17b30e8b908cb645207283f7118a189f
christo <christopher.pkm@gmail.com>
2025-01-12 16:49:50 +0000

    updated function api to work with 1d array or 2d array representations of 9x9 puzzle

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { solveProperSudoku } from "./sudokuSolver";
 
-const puzzleInput: number[][] = [
+const puzzleInput2dArr: number[][] = [
   [9, 1, 5, -1, -1, 3, 4, -1, 6],
   [-1, -1, -1, 1, -1, 2, -1, 8, 9],
   [-1, 6, -1, -1, -1, 4, 7, -1, 3],
@@ -14,15 +14,30 @@ const puzzleInput: number[][] = [
   [4, -1, 9, -1, -1, 1, 3, -1, 5],
 ];
 
+
+const puzzleInput1dArr: number[] = [
+ 9, 1, 5, -1, -1, 3, 4, -1, 6,
+ -1, -1, -1, 1, -1, 2, -1, 8, 9,
+ -1, 6, -1, -1, -1, 4, 7, -1, 3,
+
+ -1, -1, -1, 3, 1, -1, -1, 9, -1,
+ 5, -1, 8, -1, 4, -1, -1, 3, 2,
+ 3, 4, 1, 8, -1, -1, -1, 5, -1,
+
+ -1, -1, -1, 4, 9, 6, -1, -1, -1,
+ 2, 7, -1, -1, -1, -1, 9, -1, -1,
+ 4, -1, 9, -1, -1, 1, 3, -1, 5,
+];
+
 console.log("sudoku puzzle input");
-const formattedPuzzleInputAsString = puzzleInput
+const formattedPuzzleInputAsString = puzzleInput2dArr
   .map((row) => row.map((cell) => (cell === -1 ? " " : cell)).join(" "))
   .join("\n");
 console.log(formattedPuzzleInputAsString);
 
 console.log("\n");
 
-const solvedSudoku = solveProperSudoku(puzzleInput);
+const solvedSudoku = solveProperSudoku(puzzleInput2dArr);
 console.log("solved sudoku puzzle");
 const formattedResultAsString = solvedSudoku
   ?.map((row) => row.join(" "))
